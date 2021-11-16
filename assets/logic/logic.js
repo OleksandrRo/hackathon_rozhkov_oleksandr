d102aefd6891a93efc24376828e8f3ded095284f
Gabe L'Herault <glherault@tcmsolutions.net>
2021-11-16 15:49:55 -0600

    game is mostly functional, points, highscores, localstorage and css still need work

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -2,63 +2,22 @@ let startBtn = document.querySelector("#start");
 let resetBtn = document.querySelector("#reset");
 let highBtn = document.querySelector("#high-scores");
 
-let quizLordAsks = document.querySelector("#quizlord");
 let timerCnt = document.querySelector("#timer");
-
-let buttonA = document.querySelector("#buttonA");
-let buttonB = document.querySelector("#buttonB");
-let buttonC = document.querySelector("#buttonC");
-let buttonD = document.querySelector("#buttonD");
 
 let playerId = document.querySelector("#playername");
 let hiScores = document.querySelector("#highscores");
+
 let intervalGb;
 let timeLeft;
 
 let question;
 let wrongBtn;
+let points = 0;
 
-resetBtn.addEventListener("click", function(){
-    clearInterval(intervalGb);
-    timerCnt.textContent = 60;
-})
-
-startBtn.addEventListener("click", function(){
-    clearInterval(intervalGb); 
-    timeLeft = 60;    
-    startTimer();
-    quizLord();  
-})
-  
-
-function startTimer(){
-  
-  intervalGb = setInterval(function(){
-    timeLeft--;
-    timerCnt.textContent = timeLeft;
-      if(timeLeft === 0){
-        clearInterval(intervalGb);
-      }  
-    }, 1000);
-  }
-  
-  wrongBtn.addEventListener("click", function(){
-    if (timeLeft >= 5) {
-       timeLeft -= 5;
-    } else if (timeLeft < 5) {
-      timerCnt.textContent = "Quiz Lord Wins";
-      clearInterval(intervalGb);
-    }
-)}
-
-function quizLord() {
-  quizLordAsks.textContent = questionArray[question].question;
-  buttonA.textContent = questionArray[answers][0];
-  buttonB.textContent = questionArray[answers][1];
-  buttonC.textContent = questionArray[answers][2];
-  buttonD.textContent = questionArray[answers][3];
-}
-
+let buttonA = document.getElementById("0");
+let buttonB = document.getElementById("1");
+let buttonC = document.getElementById("2");
+let buttonD = document.getElementById("3");
 
 let questionArray = [
   {
@@ -73,37 +32,135 @@ let questionArray = [
   },
   {
     question:"Can you go over that _____",
-    answers: ["Octopus French Fry", "One more time", "whip and nay nay", "with blue paint"],
+    answers: ["Octopus French Fry", "one more time", "whip and nay nay", "with blue paint"],
     correctAnswer: 1
   },
   {
     question:"All your base are _____",
-    answers: ["Octopus French Fry", "One more time", "whip and nay nay", "with blue paint"],
-    correctAnswer: 2
+    answers: ["belong to us", "such a vibe", "better than cookies", "better than no base"],
+    correctAnswer: 0
   },
-]
+];
+
+function startTimer(){
+  
+  intervalGb = setInterval(function(){
+    timeLeft--;
+    timerCnt.textContent = timeLeft;
+      if(timeLeft === 0){
+        clearInterval(intervalGb);
+      }  
+    }, 1000);
+  }
+
+resetBtn.addEventListener("click", function(){
+    clearInterval(intervalGb);
+    timerCnt.textContent = 60;
+
+});
+
+let currentQuestionIndex = 0
+
+startBtn.addEventListener("click", function(){
+    clearInterval(intervalGb); 
+    timeLeft = 60;    
+    startTimer();
+    displayQuestion();
+       
+});
+
+function dispHigh(params) {
+  alert('facehead');
+}
+
+buttonA.addEventListener("click", answerSubmit);
+buttonB.addEventListener("click", answerSubmit);
+buttonC.addEventListener("click", answerSubmit);
+buttonD.addEventListener("click", answerSubmit);
+
+function correctWin() {
+  if (currentQuestionIndex === 3) {
+    dispHigh();
+    
+  } else if (timeLeft >= 0) {
+      points += 10;
+      currentQuestionIndex ++;
+      displayQuestion();
+    }  
+}
+
+function answerSubmit(eV) {
+  let clicked = eV.currentTarget;
+  if (clicked.id == questionArray[currentQuestionIndex].correctAnswer) {
+    correctWin();
+    } else {
+      wrongAnswer();
+    }
+}
+
+
+
+function wrongAnswer() {
+  if (currentQuestionIndex === 3 || timeLeft <= 0) {
+    displayPoop ();
+  } else (timeLeft -= 5) {
+    currentQuestionIndex ++;
+    displayQuestion ();
+   }
+} 
+
+function displayPoop() {
+  if (timeLeft >= 5) {
+    timeLeft -= 5; 
+ } else if (timeLeft < 5) {
+   timerCnt.textContent = "Quiz Lord Wins";
+   clearInterval(intervalGb);
+ }
+}
+
+function displayQuestion(params) {
+  let questionText = document.getElementById("questiontext");
+    questionText.textContent = questionArray[currentQuestionIndex].question;
+    buttonA.innerText = questionArray[currentQuestionIndex].answers[0];
+    buttonB.innerText = questionArray[currentQuestionIndex].answers[1];
+    buttonC.innerText = questionArray[currentQuestionIndex].answers[2];
+    buttonD.innerText = questionArray[currentQuestionIndex].answers[3];    
+}
+// function quizLord(clkEV) {
+  
+// }
+
+
+
+
+// for (let i = 0; i <question.length; i++) {
+//     console.log(question.length);
+//     currentQuestion = question[i];
+//     console.log(currentQuestion);
+// }
+
 
 // if wrongBtn.textContent = questionArray[][correctAnswer]
 
-document.addEventListener('click' )
+// document.addEventListener('click' )
 
-const question = questionArray[currentQuestion];
+// const question = questionArray[currentQuestion];
 
-if (rightAnswer) {
+// if (rightAnswer) {
   
-}
-let correctAnswers = questions[correctAnswer][];
+// }
+// let correctAnswers = questions[correctAnswer][];
 
-for (let i = 0; i < question.length; i++) {
-  currentQuestion = question[i];
-}
+// for (let i = 0; i < question.length; i++) {
+//   currentQuestion = question[i];
+// }
 
-questionArray[i].answers
-for (let = 0; i < array.lenth; i++) {
+// questionArray[i].answers
+// for (let = 0; i < array.lenth; i++) {
 
-}  
-let currentQuestion = 0;
+// }  
+// let currentQuestion = 0;
 
 
-document.querySelector("#question").textContent = questions[currentQuestion];
+// document.querySelector("#question").textContent = questions[currentQuestion];
 
