77a7bededd163e74da87857f260ea0bf1b3fcef8
OleksandrRo <roghkovaa@gmail.com>
2022-09-23 09:17:10 +0200

    change some logik and disigne

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,11 +1,10 @@
-let startBtn = document.querySelector("#start");
-let resetBtn = document.querySelector("#reset");
-let highBtn = document.querySelector("#high-scores");
-let questionText = document.getElementById("questiontext");
-let timerCnt = document.querySelector("#timer");
+const startBtn = document.querySelector("#start");
+const resetBtn = document.querySelector("#reset");
+const highBtn = document.querySelector("#high-scores");
+const questionText = document.getElementById("questiontext");
+const timerCnt = document.querySelector("#timer");
 
-// let playerId = document.querySelector("#playername");
-let hiScores = document.querySelector("#highscores");
+const hiScores = document.querySelector("#highscores");
 
 let intervalGb;
 let timeLeft;
@@ -15,91 +14,112 @@ let wrongBtn;
 let scoreKeeper = document.getElementById("player-score");
 let poiNts = 0;
 
-let buttonA = document.getElementById("0");
-let buttonB = document.getElementById("1");
-let buttonC = document.getElementById("2");
-let buttonD = document.getElementById("3");
+const buttonA = document.getElementById("0");
+const buttonB = document.getElementById("1");
+const buttonC = document.getElementById("2");
+const buttonD = document.getElementById("3");
 
-let questionArray = [
+const questionArray = [
   {
-    question:"Electric Boogaloo is what?",
-    answers: ["A common filler word used by tech school instructors", "Grilled salmon with pepper and lemon", "A tiktok sensation", "The greatest sequel of all time"],
+    question: "What is the correct command to create a new React project?",
+    answers: [" npm create-react-app myReactApp", " npm create-react-app", " npx create-react-app", " npx create-react-app myReactApp"],
     correctAnswer: 3
   },
   {
-    question:"_____ haze, all in my brain",
-    answers: ["Hazey", "Purple", "Fluffy", "Purpy"],
+    question: "What command is used to start the React local development server?",
+    answers: ["npm run dev", "npm start", "npm build", "npx install"],
     correctAnswer: 1
   },
   {
-    question:"Can you go over that _____",
-    answers: ["Octopus French Fry", "one more time", "whip and nay nay", "with blue paint"],
+    question: "What is the default local host port that a React development server uses?",
+    answers: ["8080", "3000", "3001", "5050"],
     correctAnswer: 1
   },
   {
-    question:"All your base are _____",
-    answers: ["belong to us", "such a vibe", "better than cookies", "better than no base"],
+    question: "What tool does React use to compile JSX?",
+    answers: [ "Babel", "JSX Comliler", "ReactDOM", "React Router"],
     correctAnswer: 0
   },
   {
-    question:"I just wanted to pet ______",
-    answers: ["the shelf", "the tacos", "the wabbits", "the other thing before I ate these tacos"],
+    question: "What is the children prop?",
+    answers: [" A property that lets you nest components in other components", " A property that lets you pass data to child components", " A property that lets you set an object as a property", " A property that adds child values to state"],
+    correctAnswer: 1
+  },
+  {
+    question: "React component names must begin with an uppercase letter.",
+    answers: ["True", "False", "What uppercase?"],
+    correctAnswer: 0
+  },
+  {
+    question: "Which operator can be used to conditionally render a React component?",
+    answers: [ "$", "??", "&&", "||"],
     correctAnswer: 2
   },
   {
-    question:"J-Qwery is _____",
-    answers: ["the dude slanging leftover chipotle behind starbucks", "a slang term for a life question", "a web API", "a word made up by the person who made this game"],
+    question: "When rendering a list using the JavaScript map() method, what is required for each element rendered?",
+    answers: [ "id", "index", "data", "key"],
     correctAnswer: 3
   },
   {
-    question:"In late may of 1926, Ralph Craumine invented the first _____",
-    answers: ["Toaster", "USB log", "nothing, thats complete bs", "color wheel"],
+    question: "A copy of the 'real' DOM that is kept in memory is called what?",
+    answers: [ "DOM", "Shadow DOM", "Virtual DOM  ", "React DOM"],
     correctAnswer: 2
   },
   {
-    question:"Hit it from the _____",
-    answers: ["orange beam next to the 2nd coupler", "back", "this quiz is horrible", "nacho cheese"],
+    question: "What is a common use case for ref?",
+    answers: [ "To refer to another JavaScript file", "To refer to a function", "To bind the function", "To directly access a DOM node"],
+    correctAnswer: 3
+  },
+  {
+    question: "Which of the following is NOT a rule for React Hooks?",
+    answers: [ "Hooks can only be called inside React Function components", "Hooks can be called in Class or Function components", "Hooks can only be called at the top level of a component", "Hooks cannot be conditional"],
     correctAnswer: 1
   },
+  {
+    question: "Why should you avoid copying the values of props into a component's state?",
+    answers: [ "Because that would create two instances of the same state that could become out of sync  ", "Because prop values cannot be stored in state", "Because you should never mutate state", "Because you want to allow data to flow back up to the parent"],
+    correctAnswer: 0
+  },
+  
 ];
 
-function startTimer(){
-  
-  intervalGb = setInterval(function(){
+const startTimer = () => {
+
+  intervalGb = setInterval(() => {
     timeLeft--;
     timerCnt.textContent = timeLeft;
-      if(timeLeft === 0){
-        clearInterval(intervalGb);
-      }  
-    }, 1000);
-  }
+    if (timeLeft === 0) {
+      clearInterval(intervalGb);
+    }
+  }, 1000);
+}
 
-resetBtn.addEventListener("click", function(){
-    clearInterval(intervalGb);
-    timerCnt.textContent = "";
-    buttonA.setAttribute("style", "visibility: hidden");
-    buttonB.setAttribute("style", "visibility: hidden");
-    buttonC.setAttribute("style", "visibility: hidden");
-    buttonD.setAttribute("style", "visibility: hidden");
+resetBtn.addEventListener("click", () => {
+  localStorage.clear()
+  location.reload()
 
+      
+  
+    });
+
+currentQuestionIndex = 0
+
+startBtn.addEventListener("click", () => {
+  
+  clearInterval(intervalGb);
+  timeLeft = 60;
+  poiNts = 0;
+  startTimer();
+  currentQuestionIndex = 0;
+  buttonA.setAttribute("style", "visibility: visible");
+  buttonB.setAttribute("style", "visibility: visible");
+  buttonC.setAttribute("style", "visibility: visible");
+  buttonD.setAttribute("style", "visibility: visible");
+  displayQuestion();
+  hiScores.setAttribute("style", "display: none");
 });
 
-let currentQuestionIndex = 0
-
-startBtn.addEventListener("click", function(){
-    clearInterval(intervalGb); 
-    timeLeft = 60;
-    poiNts = 0;
-    startTimer();
-    currentQuestionIndex = 0;
-    buttonA.setAttribute("style", "visibility: visible");
-    buttonB.setAttribute("style", "visibility: visible");
-    buttonC.setAttribute("style", "visibility: visible");
-    buttonD.setAttribute("style", "visibility: visible");
-    displayQuestion();
-});
-
-function dispHigh(params) {
+const dispHigh = () => {
   clearInterval(intervalGb);
   questionText.textContent = ("");
   buttonA.setAttribute("style", "visibility: hidden");
@@ -110,7 +130,7 @@ function dispHigh(params) {
   saveHighScore();
 }
 
-quizLord = document.getElementById("iamquizLORD");
+quizLord = document.getElementById("quizLORD");
 
 buttonA.addEventListener("click", answerSubmit);
 buttonB.addEventListener("click", answerSubmit);
@@ -121,54 +141,54 @@ function answerSubmit(userChoice) {
   let clicked = userChoice.currentTarget;
   if (clicked.id == questionArray[currentQuestionIndex].correctAnswer) {
     rightAnswer();
-    } else {
-      wrongAnswer();
-    }
+  } else {
+    wrongAnswer();
+  }
 }
 
-function rightAnswer() {
-  if (currentQuestionIndex === 7) {    
-    dispHigh();    
+const rightAnswer = () => {
+  if (currentQuestionIndex === 11) {
+    dispHigh();
   } else if (timeLeft >= 0) {
-      poiNts += 1000;
-      currentQuestionIndex ++;
-      quizLord.src = "./assets/images/QuizLord.png"; 
-      displayQuestion();
-    }  
+    poiNts += 1000;
+    currentQuestionIndex++;
+    quizLord.src = "./assets/images/burt.gif";
+    displayQuestion();
+  }
 }
 
-function wrongAnswer() {
-  if (currentQuestionIndex === 7 || timeLeft <= 0) {
-    dispHigh ();
+const wrongAnswer = () => {
+  if (currentQuestionIndex === 11 || timeLeft <= 0) {
+    dispHigh();
   } else if (timeLeft -= 5) {
-    currentQuestionIndex ++;
-    quizLord.src = "./assets/images/quizLordWrong.png";
-    displayQuestion ();
-   }
-} 
+    currentQuestionIndex++;
+    quizLord.src = "./assets/images/ha-ha.gif";
+    displayQuestion();
+  }
+}
 
 function saveHighScore() {
   hiScores.setAttribute("style", "display: flex");
 }
 
 
-function displayQuestion(params) {
-    questionText.textContent = questionArray[currentQuestionIndex].question;
-    buttonA.innerText = questionArray[currentQuestionIndex].answers[0];
-    buttonB.innerText = questionArray[currentQuestionIndex].answers[1];
-    buttonC.innerText = questionArray[currentQuestionIndex].answers[2];
-    buttonD.innerText = questionArray[currentQuestionIndex].answers[3];
-    scoreKeeper.textContent = `${poiNts}`;     
+const displayQuestion = () => {
+  questionText.textContent = questionArray[currentQuestionIndex].question;
+  buttonA.innerText = questionArray[currentQuestionIndex].answers[0];
+  buttonB.innerText = questionArray[currentQuestionIndex].answers[1];
+  buttonC.innerText = questionArray[currentQuestionIndex].answers[2];
+  buttonD.innerText = questionArray[currentQuestionIndex].answers[3];
+  scoreKeeper.textContent = `${poiNts}`;
 }
 
 
 const noteInput = document.getElementById("note-input");
 const noteSubmit = document.getElementById("note-submit");
 const notes = document.getElementById("notes");
-let notesStorage = JSON.parse(localStorage.getItem("notes"))? JSON.parse(localStorage.getItem("notes")): [];
+let notesStorage = JSON.parse(localStorage.getItem("notes")) ? JSON.parse(localStorage.getItem("notes")) : [];
 
 
-hiScores.addEventListener("submit", function(e) {
+hiScores.addEventListener("submit", (e) => {
   e.preventDefault();
   let currentScore = {
     initials: noteInput.value,
@@ -188,9 +208,13 @@ const listBuilder = () => {
   notes.innerHTML = "";
   notesStorage.forEach(initialsScore => {
     const note = document.createElement("li");
-  note.innerHTML = initialsScore.initials + " <------> " + initialsScore.score;
-  notes.appendChild(note);
+    note.innerHTML = initialsScore.initials + " <------> " + initialsScore.score;
+    notes.appendChild(note);
+    console.log(note)
+
   });
+ 
 };
 
 listBuilder();
+
